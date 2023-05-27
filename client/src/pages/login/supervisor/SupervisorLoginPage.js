03607a135a17f60cb64a056ec2807518bd460e44
Robert Doe <robertdoe60@gmail.com>
2023-05-27 15:23:30 +0000

    Correction of redirection

--- client/src/pages/login/supervisor/SupervisorLoginPage.js
+++ client/src/pages/login/supervisor/SupervisorLoginPage.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import style from './SupervisorLoginPage.module.css'
 import loginBranding from '../nkabom_login_dark_bg.png'
 import axios from "axios";
 
 function SupervisorLoginPage(props) {
+    const staffIdRef=useRef()
+    const passKeyRef=useRef()
+
     const apiUrl = process.env.REACT_APP_API_URL;
 
     const loginHandler=()=>{
-        axios.post(`${apiUrl}/supervisors/login/`)
+        axios.post(`${apiUrl}/supervisors/login/`, {
+            staffId:staffIdRef.current.value,
+            passKey: passKeyRef.current.value
+        })
             .then(r => {
 
         })
@@ -20,11 +26,11 @@ function SupervisorLoginPage(props) {
                 <img src={loginBranding}  width={250} alt=""/>
                 <div>
                     <label className="" htmlFor="Username">Email or Username</label>
-                    <input className="" id="Username" name="Username" type="text"/>
+                    <input className="" id="Username" ref={staffIdRef} name="Username" type="text"/>
                 </div>
                 <div>
                     <label className="" htmlFor="Password">Password</label>
-                    <input id="Password" maxLength="128" name="Password" type="password"/>
+                    <input id="Password" maxLength="128" ref={passKeyRef} name="Password" type="password"/>
                 </div>
                 <button type="submit" id="login" className={style.signinBtn}>Sign in</button>
             </form>
